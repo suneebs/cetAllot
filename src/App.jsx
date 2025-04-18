c29def34215a28f0c89df26a25f098c2a0f652c5
marshadn <muhamedmarshad18@gmail.com>
2025-04-18 07:57:37 +0530

    Add Route for Programs

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@
   import { ThemeProvider } from "next-themes";
   import Navbar from './components/Navbar';
   import Footer from './components/Footer';
+  import 'leaflet/dist/leaflet.css';
 
   import Home from './app/Home';
   import About from './app/About';
@@ -11,6 +12,7 @@
   import Eligibility from './app/Eligibility';
   import Forms from './app/Forms';
 import Apply from './app/Apply';
+import Programs from './app/Programs';
 import Login from './app/admin/Login'
 import Dashboard from './app/admin/Dashboard'
 import Contact from './app/Contact';
@@ -23,7 +25,7 @@ import Contact from './app/Contact';
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="/programs" element={<Home />} />
+          <Route path="/programs" element={<Programs />} />
           <Route path="/admission" element={<Admission />} />
           <Route path="/eligibility" element={<Eligibility />} />
           <Route path="/apply" element={<Apply />} />
